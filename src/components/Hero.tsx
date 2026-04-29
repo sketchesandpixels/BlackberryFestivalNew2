52a54faa2e9c358279114586609772cba6b0d1cb
sketchesandpixels <111024004+sketchesandpixels@users.noreply.github.com>
2026-04-29 01:11:15 -0500

    block updated

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -19,7 +19,7 @@ export const Hero = () => {
             className="mb-6"
           >
             <span className="inline-block px-6 py-2 rounded-full bg-slate-900 text-white text-xs font-black uppercase tracking-[0.3em] mb-4">
-              Summer 2026 • Inaugural Event
+              May 26, 2026 • Inaugural Event
             </span>
           </motion.div>
 
