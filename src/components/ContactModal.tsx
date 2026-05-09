70f81a43a2acee2e1181d9736ab03a4e1fc68a51
sketchesandpixels <111024004+sketchesandpixels@users.noreply.github.com>
2026-05-09 02:07:07 -0500

    contact updates

--- src/components/ContactModal.tsx
+++ src/components/ContactModal.tsx
@@ -81,9 +81,8 @@ export const ContactModal = ({ isOpen, onClose }: ContactModalProps) => {
                     <div>
                       <p className="text-[10px] font-black uppercase tracking-widest text-slate-400 mb-1">Location</p>
                       <p className="text-xl md:text-2xl font-display font-bold text-slate-900">
-                        44500 US Business 77484
                         Prairie View, Texas<br/>
-                        Blackberry Festival
+                        Festival Grounds
                       </p>
                     </div>
                   </div>
