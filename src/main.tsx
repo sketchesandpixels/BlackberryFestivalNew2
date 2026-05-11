6be39340177140bfa2029c4769e6f1b294c8f2b3
sketchesandpixels <111024004+sketchesandpixels@users.noreply.github.com>
2026-05-11 01:34:13 -0500

    updated links

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,3 @@
-// Workaround for "Cannot set property fetch of #<Window> which has only a getter"
-// This must run before any other imports that might attempt to overwrite fetch
-if (typeof window !== 'undefined') {
-  try {
-    const originalFetch = window.fetch;
-    Object.defineProperty(window, 'fetch', {
-      get: () => originalFetch,
-      set: () => {
-        console.warn('Attempted to overwrite window.fetch, but it is read-only in this environment.');
-      },
-      configurable: true,
-      enumerable: true
-    });
-  } catch (e) {
-    // If we can't redefine it, it's likely already locked or we don't have permission
-    // but at least we tried to intercept the setter.
-  }
-}
-
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
