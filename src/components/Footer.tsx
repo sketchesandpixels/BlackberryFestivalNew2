1fede115ca67ddc2456e6582a3e7018541c1f7f2
sketchesandpixels <111024004+sketchesandpixels@users.noreply.github.com>
2026-05-18 17:40:34 -0500

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -26,7 +26,7 @@ export const Footer = ({ onOpenContact, onOpenPrivacy }: FooterProps) => {
             <span className="text-xs font-black uppercase tracking-[0.4em] text-slate-400 block mt-4">Hosted by Prairie View Alumnae Chapter of Delta Sigma Theta Sorority, Inc.</span>
           </div>
           <div><span className="text-xs font-black uppercase tracking-[0.4em] text-slate-400 block mt-4">Share the Blackberry Festival Vibe</span></div>
-
+<div></div>
           <div className="flex items-center justify-center gap-12 mb-16">
                 
 
